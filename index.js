c169c616ab5fbd981f0a63411f21eadb07a78cc4
Daniel Zhang <dzhangwh@gmail.com>
2021-11-15 13:36:13 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log (string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log (string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+let x;
+switch (string) {
+    case string.toLowerCase():
+        return "I can't hear you!";
+    }   
+    switch (string) {
+        case string.toUpperCase():
+            return "YES INDEED!";
+        }   
+        switch (string) {
+            case "I love you, Grandma.":
+                return "I love you, too.";
+            }   
+
+}
+
+
